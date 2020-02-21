9bac108511459fffe0bf85d03ffc5776cb5132fd
Ubuntu <ubuntu@ip-172-31-23-174.ec2.internal>
2020-02-21 10:01:55 +0000

    deploy to heroku

--- lifestyle/settings.py
+++ lifestyle/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -87,12 +88,13 @@ WSGI_APPLICATION = 'lifestyle.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#    }
+#}
+
+DATABASES = {'default': dj_database_url.parse(os.environ["DATABASE_URL"])}
 
 
 # Password validation
@@ -154,3 +156,4 @@ STRIPE_PUBLISHABLE_KEY = os.environ.get('STRIPE_PUBLISHABLE_KEY')
 STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
