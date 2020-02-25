8bb9257fec15bb8d07acb414f7fe7a5d4d804a51
Ubuntu <ubuntu@ip-172-31-23-174.ec2.internal>
2020-02-25 19:09:35 +0000

    added forms.py to include email, first and last name in signup.html

--- users/views.py
+++ users/views.py
@@ -1,14 +1,37 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login, authenticate
-from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
+from .forms import SignUpForm
+
 
 
 # Create your views here.
 
+
+# def signup_view(request):
+#     form = SignUpForm(request.POST)
+#     if form.is_valid():
+#         form.save()
+#         username = form.cleaned_data.get('username')
+#         password = form.cleaned_data.get('password1')
+#         user = authenticate(username=username, password=password)
+#         login(request, user)
+#         return redirect('home')
+#     else:
+#         form = SignUpForm()
+#     return render(request, 'signup.html', {'form': form})
+
+
+@login_required
+def home(request):
+    return render(request, 'home.html')
+
+
+
 def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
@@ -17,7 +40,7 @@ def signup(request):
             login(request, user)
             return redirect('home')
     else:
-        form = UserCreationForm()
+        form = SignUpForm()
     return render(request, 'signup.html', {'form': form})
 
 
