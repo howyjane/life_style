ba81d51da71c41856fd5b859138eb868e4ec51ae
Ubuntu <ubuntu@ip-172-31-23-174.ec2.internal>
2020-02-29 21:13:44 +0000

    updated all static images

--- checkout/models.py
+++ checkout/models.py
@@ -1,24 +1,24 @@
-from django.db import models
-from pyuploadcare.dj.models import ImageField
+# from django.db import models
+# from pyuploadcare.dj.models import ImageField
 
-# Create your models here.
+# # Create your models here.
 
-class Course(models.Model):
-    title = models.CharField(blank=False, max_length=255)
-    desc = models.TextField(blank=False)
-    number_of_minutes = models.IntegerField(blank=False)
-    instructor = models.ForeignKey('Instructor', blank=True, null=True, on_delete=models.SET_NULL)
+# class Course(models.Model):
+#     title = models.CharField(blank=False, max_length=255)
+#     desc = models.TextField(blank=False)
+#     number_of_minutes = models.IntegerField(blank=False)
+#     instructor = models.ForeignKey('Instructor', blank=True, null=True, on_delete=models.SET_NULL)
     
-    image = ImageField(null=True)
-    cost = models.FloatField(blank=False)
+#     image = ImageField(null=True)
+#     cost = models.FloatField(blank=False)
     
-    def __str__(self):
-        return self.title
+#     def __str__(self):
+#         return self.title
     
-class Instructor(models.Model):
-    first_name = models.CharField(blank=False, max_length=100)
-    last_name = models.CharField(blank=False, max_length=100)
+# class Instructor(models.Model):
+#     first_name = models.CharField(blank=False, max_length=100)
+#     last_name = models.CharField(blank=False, max_length=100)
     
-    def __str__(self):
-        return self.first_name + " " + self.last_name
+#     def __str__(self):
+#         return self.first_name + " " + self.last_name
 
